7e25265d443a455eba81445e354dbb022aeeab8e
theiann <2015iann@gmail.com>
2025-04-17 17:06:44 -0500

    Init the project

---
+++ src/game/Game.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Game {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/maze/CellComponents.java
@@ -0,0 +1,7 @@
+package maze;
+
+public enum CellComponents {
+	WALL,
+	APERTURE,
+	EXIT
+}
