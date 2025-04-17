2d2ff1cc76b7a0828c704402354c6ba52898fbca
theiann <2015iann@gmail.com>
2025-04-17 17:30:16 -0500

    Added Movement enum and moved main to its own class

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,5 @@
 package game;
 
 public class Game {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 }
---
+++ src/game/Movement.java
@@ -0,0 +1,8 @@
+package game;
+
+public enum Movement {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
