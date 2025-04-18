4c55eaa52dc9d45cea8a773ba12bd18fb52cbb31
theiann <2015iann@gmail.com>
2025-04-18 13:41:09 -0500

    Added getter, setter, and toString methods for the class Row.

--- src/maze/Row.java
+++ src/maze/Row.java
@@ -5,10 +5,32 @@ import java.util.ArrayList;
 public class Row {
 	private ArrayList<Cell> cells;
 	
+	
+	
+	
 	public Row(ArrayList<Cell> list) {
 		this.cells = new ArrayList<>();
-		for(int i = 0; i < list.size(); i++) {
-			this.cells.add(list.get(i));
+		for (int i = 0; i < list.size(); i++) {
+			cells.add(list.get(i));
 		}
+		
 	}
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> list) {
+		if (list != null) {
+			this.cells.clear();
+			for (int i = 0; i < list.size(); i++) {
+				cells.add(list.get(i));
+			}
+		} else {this.cells = null;}
+	}
+	@Override
+	public String toString() {
+		return "Row [cells=" + cells.toString() + "]";
+	}
+
+	
 }
