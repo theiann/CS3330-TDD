eea9d20294d980fbf878e5f299e4b9afc71adc8e
jibes <jibes@Jibesh_PC>
2025-04-21 12:49:41 -0500

    added makeGrid method and CellComponents helper function to decide weather to make a side wall or aperture

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,14 @@
 package game;
 
 import java.util.Random;
+import java.util.ArrayList;
 
+import maze.Cell;
+import maze.CellComponents;
 import maze.Grid;
+import maze.Row;
 import player.Player;
+
 
 public class Game {
 	private Grid grid;
@@ -12,11 +17,64 @@ public class Game {
 	public Game() {
 		Random random = new Random();
 		int gridSize = random.nextInt(5)+3; 
-		//this.grid = makeGrid(gridSize);
-		 this.player = new Player(grid.getRows().get(gridSize - 1), grid.getRows().get(gridSize - 1).getCells().get(gridSize- 1));
+		this.grid = makeGrid(gridSize);
+		 this.player = new Player(grid.getRows().get(gridSize - 1), grid.getRows().get(gridSize - 1).getCells().get(gridSize- 1));		
+	}
+	
+	private Grid makeGrid(int gridSize) {
+		ArrayList<Row> rows= new ArrayList<>();
+		Random rand1 = new Random();
+		int exitRow = rand1.nextInt(gridSize); 
+		
+		for(int i = 0;i<gridSize; i++) {
+			ArrayList<Cell> cells = new ArrayList<>();
+			for(int j = 0; j<gridSize; j++) { 
+				CellComponents right = randomComponent(rand1); 
+				CellComponents left = randomComponent(rand1);  
+				CellComponents down = randomComponent(rand1); 
+				CellComponents up = randomComponent(rand1); 
 				
+				
+				if(j ==0) {
+					if(i==exitRow) {
+						left = CellComponents.EXIT;
+					}
+				}
+				
+				if(left != CellComponents.APERTURE && right !=CellComponents.APERTURE && up!=CellComponents.APERTURE && down != CellComponents.APERTURE && !(j ==0 && i== exitRow)){
+					int num = rand1.nextInt(4);
+					if(num == 0) {
+						up = CellComponents.APERTURE;
+					}
+					if(num ==1) {
+						down = CellComponents.APERTURE;
+					}
+					if(num == 2) {
+						left = CellComponents.APERTURE;
+					}
+					if(num == 3) {
+						right = CellComponents.APERTURE;
+					}
+					
+					
+				}
+				cells.add(new Cell(left,right,up,down));
+				
+			}
+			rows.add(new Row(cells));
+		}
+		return new Grid(rows); 
 	}
 
+	
+	private CellComponents randomComponent(Random rand1){
+	if(rand1.nextBoolean()) {
+		return CellComponents.APERTURE;
+	}
+	return CellComponents.WALL;
+	}
+	
+	
 	public Grid getGrid() {
 		return grid; 
 	}
