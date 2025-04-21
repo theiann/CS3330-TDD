d4e1f74ed9b07efd4282515d4e947a1cfb549f11
jibes <jibes@Jibesh_PC>
2025-04-21 11:51:59 -0500

    Added Game method to start game and added getters for grid and player. Still have to implement makeGrid

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,32 @@
 package game;
 
+import java.util.Random;
+
+import maze.Grid;
+import player.Player;
+
 public class Game {
+	private Grid grid;
+	private Player player;
+	
+	public Game() {
+		Random random = new Random();
+		int gridSize = random.nextInt(5)+3; 
+		//this.grid = makeGrid(gridSize);
+		 this.player = new Player(grid.getRows().get(gridSize - 1), grid.getRows().get(gridSize - 1).getCells().get(gridSize- 1));
+				
+	}
+
+	public Grid getGrid() {
+		return grid; 
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	
+	
+	
 	
 }
